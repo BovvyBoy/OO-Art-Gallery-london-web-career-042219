17ecfbebcd2ea9f52ec4c12e97725dc299108fe0
Daniel Bovington <daniel.bovington@flatironschool.com>
2019-05-20 13:14:55 +0100

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,46 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
+
+  def paintings
+    Painting.all.select { |painting|painting.artist == self  }
+  end
+
+  def galleries
+    paintings.map { |painting|painting.gallery }
+  end
+
+  def cities
+    galleries.map { |gallery|gallery.city}
+  end
+
+  def self.total_experience
+    total = Artist.all.map do |artist|
+      artist.years_experience
+    end
+    total.sum
+  end
+
+  def paintings_per_year
+    paintings.length.to_f / years_experience.to_f
+  end
+
+  def self.most_prolific
+    all.sort_by{|artist| artist.paintings_per_year}.last
+  end
+
+  def create_painting(title, price, gallery)
+    Painting.new(title, price, self, gallery)
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,6 +1,16 @@
 require_relative '../config/environment.rb'
 
+dan = Artist.new("Danny B", 15)
+bob = Artist.new("Bobby Boy", 10)
+lydia = Artist.new("Lydia", 5)
 
+vanda = Gallery.new("The V & A", "London")
+louvre = Gallery.new("The Louvre", "Paris")
+natural = Gallery.new("Natural History", "London")
+
+monalisa = Painting.new("Mona Lisa", 100, dan, vanda)
+bone = Painting.new("Big Bone", 500, bob, natural)
+face = Painting.new("Smiling Face", 10, lydia, louvre)
 
 
 binding.pry
